fe258188ca4994627c69bee21650af50066ce969
Yago Juan <yago.juan@teclat.com.br>
2021-12-27 15:12:01 -0300

    Adiciona responsividade das telas

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export default function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export default function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Yago Juan</Text>
-        <Text color="gray.300" fontSize="small">
-          yagojuan@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Yago Juan</Text>
+          <Text color="gray.300" fontSize="small">
+            yagojuan@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Yago Juan" src="https://github.com/yagoyj.png" />
     </Flex>
